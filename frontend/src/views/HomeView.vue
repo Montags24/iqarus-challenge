1fa8bb2f3141434b46d8cee51f0c1bc187d50ac2
Montags24 <adrianmontagu@hotmail.co.uk>
2024-04-15 08:14:36 +0100

    remove: boilerplate code

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,28 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div>
+    <h1>Home View</h1>
+    {{ counter }}
+    <button>Increase counter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      onLine: navigator.onLine,
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCount() {
+      if (!this.onLine) {
+        this.counter++
+      }
+    }
+  },
+
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
