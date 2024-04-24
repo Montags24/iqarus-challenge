27f4c992ce0a18d37db6c3a1a196d41f8066a156
Montags24 <adrianmontagu@hotmail.co.uk>
2024-04-24 15:19:48 +0100

    update SettingsModal.vue and MapView.vue

--- frontend/src/components/SettingsModal.vue
+++ frontend/src/components/SettingsModal.vue
@@ -3,11 +3,11 @@
         <!-- Modal -->
         <div class="fixed inset-0 flex items-center justify-center z-20">
             <div
-                class="bg-white rounded-lg p-6 w-80 sm:w-96 max-w-full shadow-lg transform transition-all duration-300">
+                class="bg-off-black text-white rounded-lg p-6 w-80 sm:w-96 max-w-full shadow-lg transform transition-all duration-300">
                 <!-- Modal Header -->
-                <div class="flex justify-between items-center border-b-2 border-gray-200 pb-4">
-                    <h2 class="text-2xl font-semibold">Rewards</h2>
-                    <button @click="toggleModal" class="text-gray-500 hover:text-gray-700 focus:outline-none">
+                <div class="flex justify-between items-center border-b-2 pb-4">
+                    <h2 class="text-2xl font-semibold">Map settings</h2>
+                    <button @click="toggleModal" class="text-white focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                             class="feather feather-x">
@@ -19,7 +19,11 @@
 
                 <!-- Modal Content -->
                 <div class="mt-6 space-y-4">
-                    <h1>Modal content</h1>
+                    <h1>Show own location</h1>
+                    <h1>Select categories to show</h1>
+                    <h1>Set timeframe</h1>
+                    <h1>Set search radius</h1>
+                    <h1>Sumbit Button</h1>
                 </div>
             </div>
         </div>
